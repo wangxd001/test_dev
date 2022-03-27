6f57dd36aa75d9097bed93547b4c8e19c393f3f7
王大锤 <1508464426@qq.com>
2022-03-27 18:31:14 +0800

    commit web

--- pytest_fixture/test_yaml.py
+++ pytest_fixture/test_yaml.py
@@ -7,6 +7,10 @@ import yaml
 
 
 class TestData:
-    @pytest.mark.parametrize('a, b', yaml.safe_load(open('./data.yaml')))
+    @pytest.mark.parametrize('a, b', yaml.safe_load(open('../data/data.yaml')))
     def test_data(self, a, b):
         print(a + b)
+
+    def test_demo(self):
+        with open('../data/demo.yaml') as file:
+            print(yaml.safe_load(file))
---
+++ web/test_selenim.py
@@ -0,0 +1,11 @@
+# coding=utf-8
+"""
+@Author: wangxd
+"""
+import selenium
+from selenium import webdriver
+
+
+def test_selenium():
+    driver = webdriver.Chrome()
+    driver.get('https://www.baidu.com')
